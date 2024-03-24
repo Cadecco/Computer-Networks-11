00140462c4ee098e5d4f41d72b30cc62c73fb855
Cadecco <127892999+Cadecco@users.noreply.github.com>
2024-03-24 16:31:18 +0000

    Creating UDP Packets

--- cudp.py
+++ cudp.py
@@ -1,24 +1,63 @@
 import socket
-import threading
-import time
-import json
 import random
+import zlib
+import struct
 
-class Packet:
-    def __init__(self, chat_id, seq_number, data, is_final, ack=False, missing_packets=None):
-        self.chat_id = chat_id
-        self.seq_number = seq_number
-        self.data = data
-        self.is_final = is_final
-        self.ack = ack
-        self.missing_packets = missing_packets if missing_packets is not None else []
+# type PcktHeader struct {
+#     Magic        uint32 // 4 bytes
+#     Checksum    uint32 // 4 bytes CRC32
+#     ConvID        uint32 // 4 bytes
+#     SequenceNum    uint32 // 4 bytes
+#     Final       uint16 // 2 bytes
+#     Type        uint16 // 2 bytes
+# }
+
+# // Types
+# const (
+#     Data            = 0 //
+#     ACK          = 1
+#     NACK           = 2
+# )
+
+# type Pckt struct {
+#     Header        PcktHeader // 20 bytes
+#     Body           []byte     // N bytes
+    
+#     // 20 + N <= 256 Bytes
+# }
+
+#define MAGIC 17109271
+
+host = '127.0.0.1'
+port = 61000
+# ID for this client, will not change once generated.
+id = random.randint(8000, 9000)
+magic = 17109271
+seq_num = 1
+final = 0
+type = 0
+
+def get_checksum(data):
+    checksum = zlib.crc32(data)
+    return checksum
+
+def create_packet(data):
+    packet = data.encode()
+    checksum = get_checksum(packet)
+
+    # Creating the UDP header with 4 fields all of 4 bytes long.
+    # One 'I' size is 4 bytes, so 4 items x 4 = 16 bytes of header.
+    header = struct.pack("!IIIIHH", magic, checksum, id, seq_num, final, type)
+
+    body = struct.pack()
+
+    udp_packet = header + packet
+    return udp_packet
+
 
 def main():
-    host = '127.0.0.1'
-    port = 61000
-    server_addr = (host, port)
 
-    id = random.randint(8000, 9000)
+    server_addr = (host, port)
 
     # Create a UDP socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -26,11 +65,15 @@ def main():
     while True:
         message = input("Enter Message: ")
 
-        Message = (f"{id} : {message} ")
+        packet = create_packet(message)
 
-        client_socket.sendto(Message.encode(), server_addr)
+        client_socket.sendto(packet, server_addr)
 
-        print("Sent message to server: {}".format(Message))
-    
+        print("Sent message to server: {}".format(message))
+
+        received, addr = client_socket.recvfrom(1024)
+        print(f"{received.decode()}")
+
+        
 if __name__ == "__main__":
     main()
--- sudp.py
+++ sudp.py
@@ -1,67 +1,108 @@
 import socket
 import threading
-import time
-import json
+import struct
+import zlib
 
 # Initialise array of chats.
 chats = {}
 
-class Chat:
-    def __init__(self, chat_id, server, data):
-        self.chat_id = chat_id
+# Declared globally here so that the server can be accessed by all the fucntions.
+host = '0.0.0.0'
+port = 61000
+   
+# Create a UDP socket
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+class udp_packet:
+    def __init__(self, magic, checksum, id, data):
+        self.magic = magic
+        self.checksum = checksum
+        self.id = id
         self.data = data
 
+
+def get_checksum(data):
+    checksum = zlib.crc32(data)
+    return checksum
+
+def corruption_check(packet):
+    correct_checksum = packet.checksum
+    checksum = get_checksum(packet.data)
+    corrupted = correct_checksum != checksum
+    return corrupted
+
+class Chat:
+    def __init__(self, addr, packet):
+        self.addr = addr
+        self.packet = packet
+        self.id = packet.id
+        self.checksum = packet.checksum
+        self.data = packet.data
         self.buffer = [] 
-        self.server = server
         self.incoming_lock = threading.Lock()  
         
-        self.thread_number = threading.Thread(target=self.chat_loop, args=(data, ))
+        self.thread_number = threading.Thread(target=self.chat_loop, args=(self.packet, ))
         self.thread_number.start()
-
   
-    def chat_loop(self, data):
+    def chat_loop(self, packet):
         with self.incoming_lock:
-            self.buffer.append(data.decode())
+            self.buffer.append(packet.data.decode())
+
+            corrupted = corruption_check(packet)
+            
             if len(self.buffer) > 0:
-                    print(f"Received message {self.chat_id}, {self.buffer[0]}")
+                    print(f"Received from ID {self.id}, {self.buffer[0]}")
                     self.buffer.pop(0)
+                    server.sendto(f"ACK".encode() , self.addr)
 
 
-def handle_client(server_socket, addr, data, chats):
+def handle_client(addr, packet, chats):
+
+    dec_pack = decode_packet(packet)
     
-    chat_number = chats.get(addr)
+    chat_number = chats.get(dec_pack.id)
 
     # If there is a chat matching this ID send it to the SR ARQ handler.
     if chat_number:
-        chats[addr].chat_loop(data)
-
+        chats[dec_pack.id].chat_loop(dec_pack)
+        
     else:
         # If the chat doesn't exist 
-        print(f"New Connecton from{addr}")
+        print(f"New Connecton from {addr} with ID: {dec_pack.id}")
 
-        chats[addr] = Chat(addr, server_socket, data)
+        chats[dec_pack.id] = Chat(addr, dec_pack)
 
         # Send the packet.
         #chats[addr].chat_loop(data)
 
     print(f"Number of Clients: {len(chats)}")
 
+# Decoding the UDP packet which is a stream of bytes.
+def decode_packet(packet):
+    # Extract everything up to and including the 16th byte as header.
+    header = packet[:20]
+    # Everything After this is the data that was sent.
+    data = packet[20:]
+    header = struct.unpack("!IIIIHH", header)
+    # The third entry in the header is the checksum.
+    magic = header[0]
+    checksum = header[1]
+    id = header[2]
+
+    new_packet = udp_packet(magic, checksum, id, data)
+    return new_packet
 
 def server_listener():
-    host = '0.0.0.0'
-    port = 61000
-   
-    # Create a UDP socket
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     
     # Bind the socket to the host and port
-    server_socket.bind((host, port))
+    server.bind((host, port))
     print("Server listening on {} : {}".format(host, port))
 
     while True:
 
-        data, addr = server_socket.recvfrom(1024)   
-        handle_client(server_socket, addr, data, chats)
+        # Receiving the UDP packet.
+        packet, addr = server.recvfrom(1024)   
+        handle_client(addr, packet, chats)
 
 def main():
     server_listener()
